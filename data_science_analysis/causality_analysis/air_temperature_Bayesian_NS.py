a68b6ea563a9310beca62dfe790aecd97a15dc54
Andrés Muñoz-Jaramillo <andres.munoz@swri.org>
2025-04-14 18:43:11 -0600

    Add coarsenging with mean to time

--- data_science_analysis/causality_analysis/air_temperature_Bayesian_NS.py
+++ data_science_analysis/causality_analysis/air_temperature_Bayesian_NS.py
@@ -1,41 +1,25 @@
 import os
-
 os.environ["CUDA_VISIBLE_DEVICES"] = "2"
 
 import sys
-
-import netCDF4 as nc
-import glob
 import numpy as np
 import pandas as pd
 import xarray as xr
-import itertools
-import cftime
-import cartopy.crs as ccrs
-from datetime import datetime
-
-import matplotlib.pyplot as plt
-import matplotlib as mpl
 
 from tqdm import tqdm
 
-import jax
-
-jax.config.update("jax_enable_x64", True)
-import jax.numpy as jnp
 from jax import random
-from jax.experimental import mesh_utils
-
-import numpyro
-
-numpyro.enable_x64()
-import numpyro.distributions as dist
-from numpyro.infer import MCMC, NUTS, Predictive
-from numpyro.contrib.nested_sampling import NestedSampler
-
-
 import dill
 import logging
+
+import jax
+jax.config.update("jax_enable_x64", True)
+import jax.numpy as jnp
+
+import numpyro
+numpyro.enable_x64()
+import numpyro.distributions as dist
+from numpyro.contrib.nested_sampling import NestedSampler
 
 log_format = logging.Formatter("[%(asctime)s] [%(levelname)s] - %(message)s")
 log_level = logging.INFO
@@ -48,8 +32,9 @@ handler.setLevel(log_level)
 handler.setFormatter(log_format)
 LOG.addHandler(handler)
 
+DATA_SMOOTHING = 30
 DATA_LABEL = "air_temperature"
-COARSEN_LEVELS = [16, 8, 4, 2]
+COARSEN_LEVELS = [4]
 CONSTRUCTOR_KWARGS = {
     "max_samples": 40000,
     "parameter_estimation": True,
@@ -61,7 +46,7 @@ DATA_INPUT_FILE = (
 )
 FIGURE_FOLDER = "/d0/amunozj/git_repos/interconnection-sun-climate/outputs/figures"
 MCMC_OUT_FOLDER = (
-    f"/d0/amunozj/git_repos/interconnection-sun-climate/outputs/nested_sampling/{DATA_LABEL}"
+    f"/d0/amunozj/git_repos/interconnection-sun-climate/outputs/nested_sampling_negative_shift/{DATA_LABEL}"
 )
 
 if not os.path.exists(FIGURE_FOLDER):
@@ -110,24 +95,24 @@ def single_lat_lon_model(
 
     # ONI
     if oni is not None:
-        oni_gain = numpyro.sample("oni_gain", dist.Normal(20.0, 40.0))
-        oni_shift = numpyro.sample("oni_shift", dist.Normal(0.0, 5.0))
+        oni_gain = numpyro.sample("oni_gain", dist.Normal(0.0,  2.0))
+        oni_shift = numpyro.sample("oni_shift", dist.Normal(0.0, 0.5))
 
         orbits_oni = (
             (oni.time - np.min(time)) / np.timedelta64(1, "D") / 365.256
         ).values
         target_variable_estimate = target_variable_estimate + oni_gain * jnp.interp(
-            orbits - oni_shift, orbits_oni, oni.normalized.values
+            orbits - jnp.abs(oni_shift), orbits_oni, oni.normalized.values
         )
 
     # Cosmic Rays
     if cr is not None:
-        cr_gain = numpyro.sample("cr_gain", dist.Normal(20.0, 40.0))
-        cr_shift = numpyro.sample("cr_shift", dist.Normal(0.0, 5.0))
+        cr_gain = numpyro.sample("cr_gain", dist.Normal(0.0,  2.0))
+        cr_shift = numpyro.sample("cr_shift", dist.Normal(0.0, 1.0))
 
         orbits_cr = ((cr.time - np.min(time)) / np.timedelta64(1, "D") / 365.256).values
         target_variable_estimate = target_variable_estimate + cr_gain * jnp.interp(
-            orbits - cr_shift, orbits_cr, cr.normalized.values
+            orbits - jnp.abs(cr_shift), orbits_cr, cr.normalized.values
         )
 
     with numpyro.plate("times", target_variable.shape[0]):
@@ -139,7 +124,7 @@ def single_lat_lon_model(
 if __name__ == "__main__":
 
     ## Read climate data
-    geo_data = xr.open_dataset(DATA_INPUT_FILE)
+    geo_data = xr.open_dataset(DATA_INPUT_FILE).sel(level=2)
 
     ## Read El Niño Index
     df = pd.read_table(
@@ -153,6 +138,7 @@ if __name__ == "__main__":
     oni_data = xr.Dataset.from_dataframe(df)
     oni_data["oni"] = oni_data.TOTAL - oni_data.ClimAdjust
     oni_data["normalized"] = (oni_data.oni - oni_data.oni.mean()) / oni_data.oni.std()
+    
 
     ## Read cosmic ray data
     crDataPath = "../../data/cosmic_rays/OULU_1964_05_01 _00_00_2024_10_28 _23_30.csv"
@@ -170,7 +156,7 @@ if __name__ == "__main__":
     for coarsen_level in pbar1:
         pbar1.set_postfix_str(f"Processing level {coarsen_level}", refresh=True)
         geo_data_coarse = geo_data.coarsen(
-            lon=coarsen_level, lat=coarsen_level, boundary="pad"
+            lon=coarsen_level, lat=coarsen_level, time=DATA_SMOOTHING, boundary="pad"
         ).mean()
 
         output_folder = os.path.join(MCMC_OUT_FOLDER, f"level_{coarsen_level}")
@@ -184,8 +170,9 @@ if __name__ == "__main__":
             leave=False,
         )
         for lon_index in pbar2:
+            lon = geo_data_coarse.lon.values[lon_index]
             pbar2.set_postfix_str(
-                f"Processing {geo_data_coarse.lon.values[lon_index]}° longitude",
+                f"Processing {lon}° longitude",
                 refresh=True,
             )
 
@@ -196,15 +183,18 @@ if __name__ == "__main__":
                 leave=False,
             )
             for lat_index in pbar3:
+                lat = geo_data_coarse.lat.values[lat_index]
                 pbar3.set_postfix_str(
-                    f"Processing {geo_data_coarse.lat.values[lat_index]}° latitude",
+                    f"Processing {lat}° latitude",
                     refresh=True,
                 )
+
+                target_variable  = geo_data_coarse.air.sel(lat=lat,lon=lon).values
 
                 # Run with only co2 increase and seasonal variation
                 output_file = os.path.join(
                     output_folder,
-                    f"{DATA_LABEL}_level{coarsen_level}_co2_season_lat_{lat_index}_lon_{lon_index}.pkl",
+                    f"{DATA_LABEL}_smoothing{DATA_SMOOTHING}_level{coarsen_level}_co2_season_lat_{lat_index}_lon_{lon_index}.pkl",
                 )
                 if not os.path.isfile(output_file):
                     ns = NestedSampler(
@@ -213,7 +203,7 @@ if __name__ == "__main__":
                     ns.run(
                         random.PRNGKey(2),
                         geo_data_coarse.time,
-                        geo_data_coarse.air.values[:, 0, lat_index, lon_index],
+                        target_variable,
                         co2=True,
                         seasonal=True,
                         oni=None,
@@ -239,7 +229,7 @@ if __name__ == "__main__":
                     ns.run(
                         random.PRNGKey(2),
                         geo_data_coarse.time,
-                        geo_data_coarse.air.values[:, 0, lat_index, lon_index],
+                        target_variable,
                         co2=True,
                         seasonal=True,
                         oni=oni_data,
@@ -265,7 +255,7 @@ if __name__ == "__main__":
                     ns.run(
                         random.PRNGKey(2),
                         geo_data_coarse.time,
-                        geo_data_coarse.air.values[:, 0, lat_index, lon_index],
+                        target_variable,
                         co2=True,
                         seasonal=True,
                         oni=None,
@@ -291,7 +281,7 @@ if __name__ == "__main__":
                     ns.run(
                         random.PRNGKey(2),
                         geo_data_coarse.time,
-                        geo_data_coarse.air.values[:, 0, lat_index, lon_index],
+                        target_variable,
                         co2=True,
                         seasonal=True,
                         oni=oni_data,
